1c77049207c4215f425b9451fc97528dd64de8c9
Yash-pfj <Yash.Vashisth@pilottravelcenters.com>
2023-07-27 15:54:00 +0530

    8th commit

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-const express = require("express");
-const app = express();
+// index.js
+const express = require('express')
 
-app.use(express.json())
+const app = express()
+const PORT = 4000
 
-app.post("/webhook", function (req, res){
-    console.log('req',req.body)
-    res.send('test response2')
-  })
-  
-app.get("/",function(req, res){
-  return res.send('my app is running')
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
 })
-let port = process.env.PORT || 9001
 
-app.listen(port,()=>{
-    console.info("test Example app listening on port 9001! Go to https://localhost:9001/" );
-});
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
 
-//https://github.com/YashVashisth772/node_server_test.git
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
 
 module.exports = app
+
+
+// const express = require("express");
+// const app = express();
+
+// app.use(express.json())
+
+// app.post("/webhook", function (req, res){
+//     console.log('req',req.body)
+//     res.send('test response2')
+//   })
+  
+// app.get("/",function(req, res){
+//   return res.send('my app is running')
+// })
+// let port = process.env.PORT || 9001
+
+// app.listen(port,()=>{
+//     console.info("test Example app listening on port 9001! Go to https://localhost:9001/" );
+// });
+
+// //https://github.com/YashVashisth772/node_server_test.git
+
+// module.exports = app
