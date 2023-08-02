6319aef945808891c83cdb34a950edd820019b73
Yash-pfj <Yash.Vashisth@pilottravelcenters.com>
2023-08-02 17:34:31 +0530

    adding sockets

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require("express");
 var cors = require('cors')
- 
-const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+var morgan = require('morgan')
 
+
+const app = express();
+const server = http.createServer(app);
+app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.json())
 
-app.post("/webhook", function (req, res){
-    console.log('req',req.body)
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+  socket.emit('notification', 'Thanks for connecting to Codedamn!')
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+app.post("/webhook", async function (req, res){    
+    const sockets = await io.fetchSockets();
+
+    sockets.forEach((socket)=>socket.emit("send_message",req.body))
     res.send(req.body)
   })
   
@@ -16,10 +37,9 @@ app.get("/",function(req, res){
 })
 let port = process.env.PORT || 9001
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.info("test Example app listening on port 9001! Go to http://localhost:9001/",port );
 });
 
-//https://github.com/YashVashisth772/node_server_test.git
 
 module.exports = app
