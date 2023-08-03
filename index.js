7086a8986e17a740df3d92dd7b17063a162c7e1f
Yash-pfj <Yash.Vashisth@pilottravelcenters.com>
2023-08-03 13:13:16 +0530

    06

--- index.js
+++ index.js
@@ -27,21 +27,23 @@ const logger = winston.createLogger({
   ],
 });
 
-app.options('*', cors()) 
+app.options('*', cors())
 
 const io = new Server(server, {
-    cors: {
-        origin: ['https://chat-app-socket-io-delta.vercel.app','http://localhost:3000'],
-    },
+  cors: { origin: "*", }, path: "/socket.io/", secure: true, timeout: 60000,
+
 });
+// cors: {
+//     origin: ['https://chat-app-socket-io-delta.vercel.app','http://localhost:3000'],
+// },
 //        allowedHeaders: ['Access-Control-Allow-Origin'],
 // methods: ["GET", "POST"],
 // credentials: true,
-        // 'force new connection': true,
+// 'force new connection': true,
 io.on("connection", (socket) => {
   logger.info(`User Connected: ${socket.id}`);
 
-  // console.log(`User Connected: ${socket.id}`);
+  console.log(`User Connected: ${socket.id}`);
   socket.emit('notification', 'Thanks for connecting to Codedamn!')
 
   socket.on("disconnect", () => {
@@ -51,22 +53,22 @@ io.on("connection", (socket) => {
     console.log("User Reconnected", socket.id);
   });
 });
-app.post("/webhook", async function (req, res){    
-    const sockets = await io.fetchSockets();
-    logger.info('test sockets',req.body,sockets.length,sockets);
-  // console.info('test sockets',sockets,sockets.length);
-    sockets.forEach((socket)=>socket.emit("send_message",req.body))
-    res.send(req.body)
-  })
-  
-app.get("/",function(req, res){
+app.post("/webhook", async function (req, res) {
+  const sockets = await io.fetchSockets();
+  logger.info('test sockets', req.body, sockets.length, sockets);
+  console.info('test sockets', sockets, sockets.length);
+  sockets.forEach((socket) => socket.emit("send_message", req.body))
+  res.send(req.body)
+})
+
+app.get("/", function (req, res) {
   return res.send('my app is running')
 })
 let port = 9001
 
-server.listen(port,()=>{
-    logger.info(`port + ${port}`);
-    console.info("test Example app listening on port 9001! Go to http://localhost:9001/",port );
+server.listen(port, () => {
+  logger.info(`port + ${port}`);
+  console.info("test Example app listening on port 9001! Go to http://localhost:9001/", port);
 });
 
 
