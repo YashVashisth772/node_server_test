9875d85f6c3d8015cd2ad773b31641edde13caf7
Yash-pfj <Yash.Vashisth@pilottravelcenters.com>
2023-08-03 12:44:05 +0530

    04

--- index.js
+++ index.js
@@ -3,33 +3,46 @@ let cors = require('cors')
 const http = require("http");
 const { Server } = require("socket.io");
 let morgan = require('morgan')
+const winston = require('winston');
 
 
 const app = express();
 const server = http.createServer(app);
 app.use(morgan('tiny'))
 app.use(express.json())
-const corsOptions ={
-  origin:['https://chat-app-socket-io-delta.vercel.app/','http://localhost:3000'], 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
+// const corsOptions ={
+//   origin:['https://chat-app-socket-io-delta.vercel.app/','http://localhost:3000'], 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200,
+// }
 
-app.use(cors(corsOptions))
-app.options('*', cors(corsOptions)) 
+// app.use(cors(corsOptions))
+app.use(cors())
+
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'my-app.log' }),
+  ],
+});
+
+// app.options('*', cors(corsOptions)) 
 
 const io = new Server(server, {
-  allowEIO3: true,
+  // allowEIO3: true,
     cors: {
-        origin: ['https://chat-app-socket-io-delta.vercel.app/','http://localhost:3000'],
-        credentials: true,
+        origin: ['https://chat-app-socket-io-delta.vercel.app','http://localhost:3000'],
+        // credentials: true,
         methods: ["GET", "POST"],
         allowedHeaders: ['Access-Control-Allow-Origin'],
-        'force new connection': true,
+        // 'force new connection': true,
     },
 });
 io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+  logger.info(`User Connected: ${socket.id}`);
+
+  // console.log(`User Connected: ${socket.id}`);
   socket.emit('notification', 'Thanks for connecting to Codedamn!')
 
   socket.on("disconnect", () => {
@@ -41,7 +54,8 @@ io.on("connection", (socket) => {
 });
 app.post("/webhook", async function (req, res){    
     const sockets = await io.fetchSockets();
-  console.info('test sockets',sockets,sockets.length);
+    logger.info('test sockets',req.body,sockets.length,sockets);
+  // console.info('test sockets',sockets,sockets.length);
     sockets.forEach((socket)=>socket.emit("send_message",req.body))
     res.send(req.body)
   })
@@ -52,6 +66,7 @@ app.get("/",function(req, res){
 let port = 9001
 
 server.listen(port,()=>{
+    logger.info(`port + ${port}`);
     console.info("test Example app listening on port 9001! Go to http://localhost:9001/",port );
 });
 
